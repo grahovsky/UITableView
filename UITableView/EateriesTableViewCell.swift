4832979015b2670a6011e6097189ac8ff30d4731
Konstantin <grahovsky@gmail.com>
2018-11-03 22:46:22 +0300

    #EateriesTableTableViewController #layer.cornerRadius #clipsToBounds #UITableViewCell

---
+++ UITableView/EateriesTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  EateriesTableViewCell.swift
+//  UITableView
+//
+//  Created by Konstantin on 03/11/2018.
+//  Copyright © 2018 Konstantin. All rights reserved.
+//
+
+import UIKit
+
+class EateriesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
