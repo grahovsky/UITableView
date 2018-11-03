499942b48680516ab43b8d1b0dd481b1f4de7da9
Konstantin <grahovsky@gmail.com>
2018-11-03 11:49:48 +0300

    #@IBAction #UIAlertController #UIAlertAction #present #constraints

--- UITableView/ViewController.swift
+++ UITableView/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        let ac = UIAlertController(title: "Hello", message: "Hello world App", preferredStyle: UIAlertController.Style.alert)
+        let alertAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+        ac.addAction(alertAction)
+        
+        present(ac, animated: true, completion: nil)
+        
+    }
+    
 }
 
