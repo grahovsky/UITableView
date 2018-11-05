84203eeacb91a6f4bcb8955b415a831b8ccb9c48
Konstantin <grahovsky@gmail.com>
2018-11-05 18:18:01 +0300

    #Navigation controller Hide Bars On Swipe #viewWillAppear #navigationController?.hidesBarsOnSwipe = false #navigationController?.setNavigationBarHidden(false, animated: true)

--- UITableView/EateriesTableViewController.swift
+++ UITableView/EateriesTableViewController.swift
@@ -27,6 +27,10 @@ class EateriesTableViewController: UITableViewController {
     Restaurant(name: "Шок", type: "ресторан", location: "Уфа", image: "shok.jpg", isVisited: false),
     Restaurant(name: "Бочка", type: "ресторан", location:  "Уфа", image: "bochka.jpg", isVisited: false)]
     
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.hidesBarsOnSwipe = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
