45e64e1cb689e6c37c633fb81ab53746aa4ca2dd
Konstantin <grahovsky@gmail.com>
2018-11-06 16:48:33 +0300

    #UIView.animate #buttonArray.enumerated()

--- UITableView/Detail/RateViewController.swift
+++ UITableView/Detail/RateViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class RateViewController: UIViewController {
 
+    @IBOutlet weak var ratingStackView: UIStackView!
+    
+    @IBOutlet weak var badButton: UIButton!
+    @IBOutlet weak var goodButton: UIButton!
+    @IBOutlet weak var brilliantButton: UIButton!
+    
+    override func viewDidAppear(_ animated: Bool) {
+//        UIView.animate(withDuration: 0.4) {
+//            self.ratingStackView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+//        }
+        
+        let buttonArray = [badButton, goodButton, brilliantButton]
+        for (index, button) in buttonArray.enumerated() {
+            let delay = Double(index) * 0.2
+            UIView.animate(withDuration: 0.6, delay: delay, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                button?.transform = CGAffineTransform(scaleX: 1, y: 1)
+            }, completion: nil)
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +41,11 @@ class RateViewController: UIViewController {
         blurEffectView.frame = self.view.bounds
         blurEffectView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         self.view.insertSubview(blurEffectView, at: 1)
+        
+//        ratingStackView.transform = CGAffineTransform(scaleX: 0, y: 0)
+        badButton.transform = CGAffineTransform(scaleX: 0, y: 0)
+        goodButton.transform = CGAffineTransform(scaleX: 0, y: 0)
+        brilliantButton.transform = CGAffineTransform(scaleX: 0, y: 0)
         
     }
     
