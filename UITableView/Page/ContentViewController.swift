840b540eee10441a3a5d82666f4965ad52b504a9
Konstantin <grahovsky@gmail.com>
2018-11-18 18:41:34 +0300

    #UserDefaults.standard

--- UITableView/Page/ContentViewController.swift
+++ UITableView/Page/ContentViewController.swift
@@ -28,6 +28,9 @@ class ContentViewController: UIViewController {
             let pageVC = parent as! PageViewController
             pageVC.nextVC(atIndex: index)
         case 1:
+            let userDefaults = UserDefaults.standard
+            userDefaults.setValue(true, forKey: "wasIntroWatched")
+            userDefaults.synchronize()
             dismiss(animated: true, completion: nil)
         default:
             break
